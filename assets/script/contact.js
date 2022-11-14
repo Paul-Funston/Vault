af3b647fa6f9f6334da8efe12fe96eaa0f0ca204
Paul Funston <paulfunston9@gmail.com>
2022-11-14 13:50:10 -0600

    basic validation

--- assets/script/contact.js
+++ assets/script/contact.js
@@ -24,28 +24,58 @@ function selectAll(selector, parent = document) {
 
 const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
 const phoneRegexNA = /^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$/;
-const formName = '';
-const formEmail = '';
-const formMessage = '';
+const formName = select('.name');
+const formEmail = select('.email');
+const formMessage = select('.message');
 const formSubscribe = '';
 const formButton = '';
 const formPhone = '';
+const formInputs = selectAll('form input');
 
 
-function displayValid (element, boolean = false) { 
-  element 
+// function displayValid (element, boolean = false) { 
+//   element 
+// }
+
+function isEmpty(input) {
+  if (input.length === 0 )
+    return true
+  return false
 }
 
-function isEmpty() {
-  
+function anyEmpty(...inputs) {
+  let result = false;
+  inputs.forEach(input => {
+    if (isEmpty(input))
+      result = true;
+  })
+  return result
 }
+
+function isEmail(input) {
+  if (emailRegex.test(input)) {
+    return true
+  }
+  return false
+}
+
+function isNaPhone(input) {
+  if (phoneRegexNA.test(input)) {
+    return true
+  }
+  return false
+}
+
 function validate () {
-  let userName = formName.trim();
-  let userEmail = formEmail.trim();
-  let userMessage = formMessage.trim();
-  let wantsSubscribe = formSubscribe.checked;
-  let userPhone = fromPhone.trim();
+  let userName = formName.value.trim();
+  let userEmail = formEmail.value.trim();
+  let userMessage = formMessage.value.trim();
+  let userPhone = fromPhone.value.trim();
 
+  if (anyEmpty(userName, userEmail, userMessage))
+    return false
 
+  
 
 }
+
